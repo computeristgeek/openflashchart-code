7cb9a28c16680852a46d0e54cf65fe85a76b9a08
jvandervort1 <jvandervort1@29cd2e10-8a35-0410-9714-db9b3f237ba7>
2008-06-25 15:37:20 +0000

    created an axis object to hold the x and y axes.

--- version-2/perl-ofc-library/open_flash_chart.pm
+++ version-2/perl-ofc-library/open_flash_chart.pm
@@ -7,37 +7,64 @@ package chart;
 sub new() {
   # Constructer for the open_flash_chart_api
   # Sets our default variables
-  my ($proto, $bootstrap) = @_;
+  my ($proto, $skip_bootstrap) = @_;
   my $class = ref($proto) || $proto;
   my $self  = {};
   bless $self, $class;
   
   $self->{'data_load_type'} = 'inline_js'; # or 'url_callback'  not sure if we still need both
   
-  $self->{'bootstrap'} = 0; # you can set this to 1 if you want to handle the includes
-  $self->{'bootstrap'} = $bootstrap if defined($bootstrap);
+  $self->{'skip_bootstrap'} = 0; # you can set this to 1 if you want to handle the includes
+  $self->{'skip_bootstrap'} = $skip_bootstrap if defined($skip_bootstrap);
   
   $self->{'chart_props'} = {
+
     "title"=>{
       "text"=>"Default Chart Title",
       "style"=>"{font-size:20px; font-family:Verdana; text-align:center;}"
     },
-    "x_axis"=>{
-      "min"=>     undef,
-      "max"=>     undef,
-      "labels"=> ["a","b","c","d","e"]
-    },
-    "y_axis"=>{
-      "min"=>     0,
-      "max"=>     "a",
-      "colour"=>  "#d09090",
-    }
+#    "x_axis"=>{
+#      "min"=>     undef,
+#      "max"=>     undef,
+#      "labels"=> ["a","b","c","d","e"]
+#    },
+#    "y_axis"=>{
+#      "min"=>     0,
+#      "max"=>     "a",
+#      "colour"=>  "#d09090",
+#    },
+  };
+  my $x = axis->new('x_axis');
+  $x->set_min(undef);
+  $x->set_max(undef);
+  $x->set_labels(["a","b","c","d","e"]);
+  
+  $self->{'axis'} = {
+  	'x_axis' => $x,
+  	'y_axis' => axis->new('y_axis')
   };
   $self->{'elements'} = [];
   
   return $self;
 }
 
+# props are at the chart level, such as axis
+sub get_axis() {
+  my ($self, $name) = @_;
+  
+  my $e=undef;
+  eval("\$e = ${name}->new();");
+  if ( defined($e) ) {
+    return $e;
+  } 
+}
+
+sub add_axis() {
+  my ($self, $prop) = @_;
+  push(@{$self->{'props'}}, $prop);
+}
+
+# elements are the data series items, usually containing values to plot
 sub get_element() {
   my ($self, $element_name) = @_;
   
@@ -59,38 +86,32 @@ sub add_element() {
 sub use_extremes {
   my ($self) = @_;
 
-	$self->{'chart_props'}->{'x_axis'}->{'min'} = 'a';
-	$self->{'chart_props'}->{'x_axis'}->{'max'} = 'a';
-	$self->{'chart_props'}->{'y_axis'}->{'min'} = 'a';
-	$self->{'chart_props'}->{'y_axis'}->{'max'} = 'a';
+  for ( keys %{$self->{'axis'}} ) {
+  	$self->{'axis'}->{$_}->{'props'}->{'max'} = 'a';
+  	$self->{'axis'}->{$_}->{'props'}->{'min'} = 'a';
+  }  
 }
 
 sub render_chart_data() {
   my ($self) = @_;
 
-  my $tmp = ''; #$self->get_page_bootstrap();
+  my $tmp = '';
 
   my $ext = $self->get_auto_extremes();
   
-  if ($self->{'chart_props'}->{'y_axis'}->{'max'} =~ /a/) {
-    $self->{'chart_props'}->{'y_axis'}->{'max'} = main::smoother($ext->{'y_max'});
-  }
-
-  if ($self->{'chart_props'}->{'x_axis'}->{'max'} =~ /a/) {
-    $self->{'chart_props'}->{'x_axis'}->{'max'} = $ext->{'x_max'};
-  }
-
-  if ($self->{'chart_props'}->{'y_axis'}->{'min'} =~ /a/) {
-    $self->{'chart_props'}->{'y_axis'}->{'min'} = $ext->{'y_min'};
-  }
-
-  if ($self->{'chart_props'}->{'x_axis'}->{'min'} =~ /a/) {
-    $self->{'chart_props'}->{'x_axis'}->{'min'} = $ext->{'x_min'};
-  }
-
-  
   $tmp .= "{";
   $tmp .= main::to_json($self->{'chart_props'});
+
+  for ( keys %{$self->{'axis'}} ) {
+  	if ($self->{'axis'}->{$_}->{'props'}->{'max'} eq 'a') {
+			$self->{'axis'}->{$_}->{'props'}->{'max'} = main::smooth_max($ext->{$_ . '_max'});
+		}
+  	if ($self->{'axis'}->{$_}->{'props'}->{'min'} eq 'a') {
+			$self->{'axis'}->{$_}->{'props'}->{'min'} = main::smooth_min($ext->{$_ . '_min'});
+		}
+    $tmp .= $self->{'axis'}->{$_}->to_json();
+  }  
+
   if ( @{$self->{'elements'}} > 0 ) {
     $tmp .= "\n".'"elements" : [';
     for my $s ( @{$self->{'elements'}} ) {
@@ -112,9 +133,9 @@ sub render_swf {
   my $html = '';
   
   if ( $self->{'data_load_type'} eq 'inline_js' ) {
-    if ($self->{'bootstrap'} == 0 ) {
+    if ($self->{'skip_bootstrap'} == 0 ) {
       $html.='<script type="text/javascript" src="swfobject.js"></script>';
-      $self->{'bootstrap'} = 1;
+      $self->{'skip_bootstrap'} = 1;
     }
     $html .= qq^
     <div id="my_chart"></div>
@@ -170,28 +191,28 @@ sub get_auto_extremes() {
   $smooth_rounding = 1 if !defined($smooth_rounding);
   $head_room = 0.1 if !defined($head_room);
   
-  my $extremes = {'x_max' => undef, 'x_min' => undef, 'y_max' => undef, 'y_min' => undef, 'other' => undef};
+  my $extremes = {'x_axis_max' => undef, 'x_axis_min' => undef, 'y_axis_max' => undef, 'y_axis_min' => undef, 'other' => undef};
     
   for my $e ( @{$self->{'elements'}} ) {
 
-    $extremes->{'x_max'} = $e->{'extremes'}->{'x_max'} if !defined($extremes->{'x_max'});
-    if ( $e->{'extremes'}->{'x_max'} > $extremes->{'x_max'} ) {
-        $extremes->{'x_max'} = $e->{'extremes'}->{'x_max'};
+    $extremes->{'x_axis_max'} = $e->{'extremes'}->{'x_axis_max'} if !defined($extremes->{'x_axis_max'});
+    if ( $e->{'extremes'}->{'x_axis_max'} > $extremes->{'x_axis_max'} ) {
+        $extremes->{'x_axis_max'} = $e->{'extremes'}->{'x_axis_max'};
     }
 
-    $extremes->{'y_max'} = $e->{'extremes'}->{'y_max'} if !defined($extremes->{'y_max'});
-    if ( $e->{'extremes'}->{'y_max'} > $extremes->{'y_max'} ) {
-        $extremes->{'y_max'} = $e->{'extremes'}->{'y_max'};
+    $extremes->{'y_axis_max'} = $e->{'extremes'}->{'y_axis_max'} if !defined($extremes->{'y_axis_max'});
+    if ( $e->{'extremes'}->{'y_axis_max'} > $extremes->{'y_axis_max'} ) {
+        $extremes->{'y_axis_max'} = $e->{'extremes'}->{'y_axis_max'};
     }
 
-    $extremes->{'x_min'} = $e->{'extremes'}->{'x_min'} if !defined($extremes->{'x_min'});
-    if ( $e->{'extremes'}->{'x_min'} < $extremes->{'x_min'} ) {
-        $extremes->{'x_min'} = $e->{'extremes'}->{'x_min'};
+    $extremes->{'x_axis_min'} = $e->{'extremes'}->{'x_axis_min'} if !defined($extremes->{'x_axis_min'});
+    if ( $e->{'extremes'}->{'x_axis_min'} < $extremes->{'x_axis_min'} ) {
+        $extremes->{'x_axis_min'} = $e->{'extremes'}->{'x_axis_min'};
     }
 
-    $extremes->{'y_min'} = $e->{'extremes'}->{'y_min'} if !defined($extremes->{'y_min'});
-    if ( $e->{'extremes'}->{'y_min'} < $extremes->{'y_min'} ) {
-        $extremes->{'y_min'} = $e->{'extremes'}->{'y_min'};
+    $extremes->{'y_axis_min'} = $e->{'extremes'}->{'y_axis_min'} if !defined($extremes->{'y_axis_min'});
+    if ( $e->{'extremes'}->{'y_axis_min'} < $extremes->{'y_axis_min'} ) {
+        $extremes->{'y_axis_min'} = $e->{'extremes'}->{'y_axis_min'};
     }
 
   }
@@ -226,7 +247,7 @@ sub get_auto_extremes() {
 #"scatter",
 
 #############################
-sub _____ELEMENTS_____(){}
+sub _____ELEMENT_OBJECTS_____(){}
 #############################
 package element;
 use Carp qw(cluck);
@@ -253,18 +274,18 @@ sub set_values {
 
 sub set_extremes {
   my ($self) = @_;
-  my $extremes = {'x_max' => undef, 'x_min' => undef, 'y_max' => undef, 'y_min' => undef, 'other' => undef};
+  my $extremes = {'x_axis_max' => undef, 'x_axis_min' => undef, 'y_axis_max' => undef, 'y_axis_min' => undef, 'other' => undef};
   for ( @{$self->{'element_props'}->{'values'}} ) {
     if ( ref($_) eq 'HASH' || ref($_) eq 'ARRAY' ) {
       return $extremes;
     }
-    $extremes->{'y_max'} = $_ if !defined($extremes->{'y_max'});
-    if ( $_ > $extremes->{'y_max'} ) {
-      $extremes->{'y_max'} = $_;
+    $extremes->{'y_axis_max'} = $_ if !defined($extremes->{'y_axis_max'});
+    if ( $_ > $extremes->{'y_axis_max'} ) {
+      $extremes->{'y_axis_max'} = $_;
     }
-    $extremes->{'y_min'} = $_ if !defined($extremes->{'y_min'});
-    if ( $_ < $extremes->{'y_min'} ) {
-      $extremes->{'y_min'} = $_;
+    $extremes->{'y_axis_min'} = $_ if !defined($extremes->{'y_axis_min'});
+    if ( $_ < $extremes->{'y_axis_min'} ) {
+      $extremes->{'y_axis_min'} = $_;
     }
   }
   $self->{'extremes'} = $extremes;
@@ -298,9 +319,9 @@ sub AUTOLOAD {
 	}
 
 	if (@_) {
-	  return $self->{'element_props'}->{$name} = shift;
+	  return $self->{'element_props'}->{"$name"} = shift;
 	} else {
-    return $self->{'element_props'}->{$name};
+    return $self->{'element_props'}->{"$name"};
 	}
 }
 sub DESTROY {  }
@@ -317,6 +338,11 @@ sub new() {
   $self->{'element_props'}->{'colour'} = main::random_color();
   $self->{'element_props'}->{'text'} = 'text';
   $self->{'element_props'}->{'font-size'} = 10;
+
+  $self->{'element_props'}->{'show_y2'} = 'false';
+  #$self->{'element_props'}->{'y2_lines'} = [];
+  $self->{'element_props'}->{'values_2'} = [];
+
   return $self;
 }
 
@@ -468,21 +494,21 @@ sub new() {
 #stackbar must override set_extremes() because of nested value list
 sub set_extremes {
   my ($self) = @_;
-  my $extremes = {'x_max' => undef, 'x_min' => undef, 'y_max' => undef, 'y_min' => undef, 'other' => undef};
+  my $extremes = {'x_axis_max' => undef, 'x_axis_min' => undef, 'y_axis_max' => undef, 'y_axis_min' => undef, 'other' => undef};
   for my $v ( @{$self->{'element_props'}->{'values'}} ) {
-    my $bar_ext = {'x_max' => undef, 'x_min' => undef, 'y_max' => undef, 'y_min' => undef, 'other' => undef};
+    my $bar_ext = {'x_axis_max' => undef, 'x_axis_min' => undef, 'y_axis_max' => undef, 'y_axis_min' => undef, 'other' => undef};
     if (ref($v) eq 'ARRAY') {
       for ( @$v ) {
         next if !defined($_->{'val'});
-        if ( !defined($bar_ext->{'y_max'}) ) {
-          $bar_ext->{'y_max'} = $_->{'val'} ;
+        if ( !defined($bar_ext->{'y_axis_max'}) ) {
+          $bar_ext->{'y_axis_max'} = $_->{'val'} ;
         } else {
-          $bar_ext->{'y_max'} += $_->{'val'};
+          $bar_ext->{'y_axis_max'} += $_->{'val'};
         }
       }
     }
-    if ( $bar_ext->{'y_max'} > $extremes->{'y_max'} ) {
-      $extremes->{'y_max'} = $bar_ext->{'y_max'};
+    if ( $bar_ext->{'y_axis_max'} > $extremes->{'y_axis_max'} ) {
+      $extremes->{'y_axis_max'} = $bar_ext->{'y_axis_max'};
     }
   }
   $self->{'extremes'} = $extremes;
@@ -502,7 +528,7 @@ sub new() {
   $self->{'element_props'}->{'colours'} = [main::random_color(), main::random_color(), main::random_color(), main::random_color(), main::random_color()];
   $self->{'element_props'}->{'border'} = 2;
   $self->{'element_props'}->{'animate'} = 1;
-  $self->{'element_props'}->{'start-angle'} = 0;	#not working?
+  $self->{'element_props'}->{'start-angle'} = 0;
   
   $self->{'element_props'}->{'values'} = [ {'value'=>rand(255), 'text'=>'linux'}, {'value'=>rand(255), 'text'=>'windows'}, {'value'=>rand(255), 'text'=>'vax'}, {'value'=>rand(255), 'text'=>'NexT'}, {'value'=>rand(255), 'text'=>'solaris'}];
 
@@ -533,29 +559,93 @@ sub new() {
 }
 sub set_extremes {
   my ($self) = @_;
-  my $extremes = {'x_max' => undef, 'x_min' => undef, 'y_max' => undef, 'y_min' => undef, 'other' => undef};
+  my $extremes = {'x_axis_max' => undef, 'x_axis_min' => undef, 'y_axis_max' => undef, 'y_axis_min' => undef, 'other' => undef};
   for ( @{$self->{'element_props'}->{'values'}} ) {
-    $extremes->{'y_max'} = $_->{'y'} if !defined($extremes->{'y_max'});
-    if ( $_->{'y'} > $extremes->{'y_max'} ) {
-      $extremes->{'y_max'} = $_->{'y'};
+    $extremes->{'y_axis_max'} = $_->{'y'} if !defined($extremes->{'y_axis_max'});
+    if ( $_->{'y'} > $extremes->{'y_axis_max'} ) {
+      $extremes->{'y_axis_max'} = $_->{'y'};
     }
-    $extremes->{'y_min'} = $_->{'y'} if !defined($extremes->{'y_min'});
-    if ( $_->{'y'} < $extremes->{'y_min'} ) {
-      $extremes->{'y_min'} = $_->{'y'};
+    $extremes->{'y_axis_min'} = $_->{'y'} if !defined($extremes->{'y_axis_min'});
+    if ( $_->{'y'} < $extremes->{'y_axis_min'} ) {
+      $extremes->{'y_axis_min'} = $_->{'y'};
     }
 
-    $extremes->{'x_max'} = $_->{'x'} if !defined($extremes->{'x_max'});
-    if ( $_->{'x'} > $extremes->{'x_max'} ) {
-      $extremes->{'x_max'} = $_->{'x'};
+    $extremes->{'x_axis_max'} = $_->{'x'} if !defined($extremes->{'x_axis_max'});
+    if ( $_->{'x'} > $extremes->{'x_axis_max'} ) {
+      $extremes->{'x_axis_max'} = $_->{'x'};
     }
-    $extremes->{'x_min'} = $_->{'x'} if !defined($extremes->{'x_min'});
-    if ( $_->{'x'} < $extremes->{'x_min'} ) {
-      $extremes->{'x_min'} = $_->{'x'};
+    $extremes->{'x_axis_min'} = $_->{'x'} if !defined($extremes->{'x_axis_min'});
+    if ( $_->{'x'} < $extremes->{'x_axis_min'} ) {
+      $extremes->{'x_axis_min'} = $_->{'x'};
     }
 
   }
   $self->{'extremes'} = $extremes;
 }
+
+#############################
+sub _____AXIS_OBJECTS_____(){}
+#############################
+package axis;
+use Carp qw(cluck);
+
+our $AUTOLOAD;
+sub new() {
+  my ($proto, $name) = @_;
+  my $class = ref($proto) || $proto;
+  my $self  = {};
+  $self->{'name'} = $name; # x_axis | y_axis | y_axis_right
+  $self->{'props'} =  {
+  	'labels' =>       undef,
+		'stroke' =>				2,
+		'tick-length' =>	3,
+		'colour' =>				'#784016',
+		'offset' =>				'false',
+		'grid-colour' =>	'#F5E1AA',
+		'3d' =>						0,
+		'steps' =>				4,
+		'visible' =>			'true',
+		'min' =>					0,
+		'max' =>					'a'
+  };
+  return bless $self, $class;
+}
+
+sub to_json() {
+  my ($self) = @_;
+  my $json = main::to_json($self->{'props'}, $self->{'name'});
+  #$json =~ s/,$//g;
+  return $json;
+}
+sub AUTOLOAD {
+	my $self = shift;
+	my $type = ref($self) or warn "$self is not an object";
+
+	my $name = $AUTOLOAD;
+	$name =~ s/.*://;   # strip fully-qualified portion
+	$name =~ s/^set_//; # strip set_
+	$name =~ s/^get_//; # strip get_
+
+	unless (exists $self->{'props'}->{$name} ) {
+	  cluck "'$name' is not a valid property in class $type";
+	  return undef;
+	}
+
+	if (@_) {
+	  return $self->{'props'}->{"$name"} = shift;
+	} else {
+    return $self->{'props'}->{"$name"};
+	}
+}
+sub DESTROY {  }
+
+
+
+
+
+
+
+
 
 
 
@@ -641,16 +731,30 @@ sub url_escape {
 # also changes number to an int
 sub smoother {
 	my $number = shift;
+	my $min_max = shift;
 	my $n = $number;
-  
- 	if ( $n < 100 ) { $n = $n + (-$n % 10) }
+	
+	if ( $min_max eq 'max' ) {
+		$n+=1;
+	} else {
+		$n-=1;
+	}
+  if ( $n <= 10 ) { $n = int($n) }
+  elsif ( $n < 30 ) { $n = $n + (-$n % 5) }
+ 	elsif ( $n < 100 ) { $n = $n + (-$n % 10) }
  	elsif ( $n < 500 ) { $n = $n + (-$n % 50) }
  	elsif ( $n < 1000 ) { $n = $n + (-$n % 100) }
  	elsif ( $n < 10000 ) { $n = $n + (-$n % 200) }
  	else { $n = $n + (-$n % 500) }
-  $n = int($n);
-  
-	return $n;
+  return int($n);
+}
+sub smooth_max {
+	my $number = shift;
+	return smoother($number, 'max');
+}
+sub smooth_min {
+	my $number = shift;
+	return smoother($number, 'min');
 }
 
 1;
